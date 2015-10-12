aed8feb3f15e508c1effb8d2dca5823c771a5966
Martin Alejandro Melo <alejandromelo2005@gmail.com>
2015-10-12 15:37:50 -0300

    pequeño cambio para que corra en heroku

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://localhost/party-dev',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/party-dev',
 	logging: {
 		format: 'tiny'	
 	},
