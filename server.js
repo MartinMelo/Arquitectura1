e0e4499e671f5d2587de080102ceb4957f3c7785
Martin Alejandro Melo <alejandromelo2005@gmail.com>
2015-10-12 17:49:12 -0300

    otro try para que heroku compile esta porqueria

--- server.js
+++ server.js
@@ -4,7 +4,6 @@
  * Cargo el ambiente necesario, por defecto es el de development.
  * @type {string}
  */
-//process.env.NODE_ENV = 'development';
 process.argv.forEach(function (val, index, array) {
     if(val === '--production'){
         process.env.NODE_ENV = 'production';
