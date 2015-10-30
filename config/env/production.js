1ac835b6ea9000584e4bb251e8cc6758e15e5b48
Martin Alejandro Melo <alejandromelo2005@gmail.com>
2015-10-30 02:49:49 -0300

    para algo estan las variables de entorno de nodejs

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://heroku_kplvh8xh:iti6i0b5k00g443r72qcjt5u49@ds041821.mongolab.com:41821/heroku_kplvh8xh',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/party',
 	assets: {
 		lib: {
 			css: [
